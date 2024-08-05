a87aabc2062632145c84d06c24e0c19a09c98a8b
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2024-08-05 20:12:39 +0200

    add: setup.sh file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.zprofile ~/.zprofile
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.ssh/config ~/.ssh/config
+ln -s ~/.dotfiles/.config/nvim/ ~/.config/nvim/
+
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
