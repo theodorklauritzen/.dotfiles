35e974d1cacad6458e81447c15089d5109e76899
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2024-09-13 22:08:35 +0200

    feat: Add Brewfile

---
+++ README.md
@@ -0,0 +1,9 @@
+# .dotfiles for Theodor
+
+## Installation on a new mac
+
+```
+xcode-select --install
+```
+
+Clone the repo to `~/.dotfiles`, then run `setup.sh`
--- setup.sh
+++ setup.sh
@@ -6,4 +6,6 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.ssh/config ~/.ssh/config
 ln -s ~/.dotfiles/.config/nvim/ ~/.config/nvim/
 
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew bundle --file ~/.dotfiles/Brewfile
