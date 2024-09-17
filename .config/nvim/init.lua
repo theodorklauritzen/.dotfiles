c4030066a6b0418967a3b7c35b6079db5722edb2
Theodor Kvalsvik Lauritzen <19690242+theodorklauritzen@users.noreply.github.com>
2024-09-17 22:05:17 +0200

    feat: Add fast navigation between buffers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -105,6 +105,10 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Buffer navigation
+vim.keymap.set('n', '<leader>n', ':bnext<CR>', { desc = '[N]ext Buffer' })
+vim.keymap.set('n', '<leader>p', ':bprev<CR>', { desc = '[P]revious Buffer'})
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
